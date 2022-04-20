0e144b78875e22a86f686511dc112473735ee513
DESKTOP-K6BD0OK\Hp <imranali999645@gmail.com>
2022-04-21 01:04:28 +0200

    updated product view

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -15,23 +15,23 @@
 
   <div class="form-group">
     <label for="exampleFormControlInput1">Product Name</label>
-    <input type="text" name='name' class="form-control" id="exampleFormControlInput1" placeholder="Category Name">
+    <input type="text" name='name' class="form-control" id="exampleFormControlInput1" placeholder="Product Name">
   </div>
   <div class="form-group mt-5">
     <label for="exampleFormControlTextarea1">Description</label>
-    <textarea class="form-control" name='description' id="exampleFormControlTextarea2" rows="5" placeholder="Description"></textarea>
+    <textarea class="form-control" name='description' id="exampleFormControlTextarea1" rows="5" placeholder="Description"></textarea>
   </div>
   <div class="form-group mt-5">
     <label for="exampleFormControlTextarea1">Product Price</label>
-    <input type="number" name='price' class="form-control" id="exampleFormControlInput1" placeholder="Product Price">
+    <input type="number" name='price' class="form-control" id="exampleFormControlInput2" placeholder="Product Price">
   </div>
   <div class="form-group mt-5">
     <label for="exampleFormControlTextarea1">Discount Price</label>
-    <input type="number" name='discountprice' class="form-control" id="exampleFormControlInput1" placeholder="Discount Price">
+    <input type="number" name='discountprice' class="form-control" id="exampleFormControlInput3" placeholder="Discount Price">
   </div>
   <div class="form-group mt-5">
     <label for="exampleFormControlTextarea1">Discount Percentage</label>
-    <input type="text" name='discountpercentage' class="form-control" id="exampleFormControlInput1" placeholder="Discount Percentage">
+    <input type="text" name='discountpercentage' class="form-control" id="exampleFormControlInput4" placeholder="Discount Percentage">
   </div>
 
   <div class="form-group mt-5">
