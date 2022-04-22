cddc6a89079bb836f52a5c00f6112efa6220de2e
Muhammad Danish <muhammadd@alec.ae>
2022-04-23 02:58:48 +0500

    add action route for form submission in product create

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-  <form class='container mt-5' action="{{ url('/admin/product/create') }}" method='GET' enctype='mutipart/form-data'>
+  <form class='container mt-5' action="{{ route('product.save') }}" method='POST' enctype='mutipart/form-data'>
     @csrf
 
   <div class="form-group">
