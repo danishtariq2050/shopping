373f0478b11fa4eb5b651a5c11530c16751c0e98
Waqar <imranali999645@gmail.com>
2022-04-24 04:47:30 +0200

    linked product index view with master file of product folder

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,18 +1,6 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <title>Categories</title>
+@extends('admin.product.master')
 
-        <!-- Bootstrap CSS -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    </head>
+@section('content')
     <body>
         <div class="container">
             <h1 class="text-center mt-3 mb-5">Products</h1>
@@ -60,4 +48,6 @@
         </div>
     </body>
 </html>
+
+@endsection
 
