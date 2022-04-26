81d83a0e036c25095fe2cc3206921e4af5bf89ce
Muhammad Danish <muhammadd@alec.ae>
2022-04-27 03:09:27 +0500

    how to add unique and nullable keys in table

--- database/migrations/2022_04_19_203951_create_products_table.php
+++ database/migrations/2022_04_19_203951_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique()->nullable();
             $table->text('description')->nullable();
             $table->double('price');
             $table->double('discountprice')->nullable();
