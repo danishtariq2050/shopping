04652b424fd4423995d16a4520d7a8914b2ba71e
Muhammad Danish <muhammadd@alec.ae>
2022-04-28 03:05:21 +0500

    design product show page

---
+++ resources/views/admin/product/show.blade.php
@@ -0,0 +1,28 @@
+@extends('master')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-center">Product Details</h1>
+        <hr>
+        <div class="row mt-5">
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-4 font-weight-bold mb-2">Product Name:</div>
+                    <div class="col-8">{{$product->name}}</div>
+
+                    <div class="col-4 font-weight-bold mb-2">Description:</div>
+                    <div class="col-8">{{$product->description}}</div>
+
+                    <div class="col-4 font-weight-bold mb-2">Price:</div>
+                    <div class="col-8">{{$product->price}}</div>
+
+                    <div class="col-4 font-weight-bold mb-2">Discount Price</div>
+                    <div class="col-8">{{$product->discountprice}}</div>
+                </div>
+            </div>
+            <div class="col-4">
+                <img src="" alt="Product Image" />
+            </div>
+        </div>
+    </div>
+@endsection
