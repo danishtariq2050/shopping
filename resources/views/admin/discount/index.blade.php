ff526b8eba6e9ac08930170b3739d277135ab6be
Waqar <imranali999645@gmail.com>
2022-05-10 20:51:24 +0200

    Added Add btn to discounted product view

--- resources/views/admin/discount/index.blade.php
+++ resources/views/admin/discount/index.blade.php
@@ -1,53 +1,65 @@
 @extends('admin.product.master')
 
 @section('content')
-    <body>
-        <div class="container">
-            <h1 class="text-center mt-3 mb-5">Discounted Products</h1>
 
-            <table class="table table-hover table-dark">
-                <thead>
-                    <tr>
-                        <th>S/N</th>
-                        <th>Image</th>
-                        <th>Product Name</th>
-                        <th>Description</th>
-                        <th>Price</th>
-                        <th>Created Date</th>
-                        <th colspan="3">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($products as $p)
-                        <tr>
-                            <td>{{$p->id}}</td>
-                            <td>{{$p->image}}</td>
-                            <td>{{$p->name}}</td>
-                            <td>{{$p->description}}</td>
-                            <td>{{$p->price}}</td>
-                            <td>{{$p->created_at}}</td>
-                            <td>
-                                <button class="btn btn-primary btn-sm">
-                                    <i class="fa fa-eye"></i>
-                                </button>
-                            </td>
-                            <td>
-                                <button class="btn btn-warning btn-sm">
-                                    <i class="fa fa-edit"></i>
-                                </button>
-                            </td>
-                            <td>
-                                <button class="btn btn-danger btn-sm">
-                                    <i class="fa fa-remove"></i>
-                                </button>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+<body>
+    <div class="container">
+        <h1 class="text-center mt-3 mb-5">Discounted Products</h1>
+
+        <div class='mb-5 d-flex justify-content-end'>
+
+            <a href="{{route('products.create')}}" target='_blank' class='btn btn-info'> + Add New</a>
+
         </div>
-    </body>
+
+
+        <table class="table table-hover table-dark">
+            <thead>
+                <tr>
+                    <th>S/N</th>
+                    <th>Image</th>
+                    <th>Product Name</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Created Date</th>
+                    <th colspan="3">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $p)
+                <tr>
+                    <td>{{$p->id}}</td>
+                    <td>{{$p->image}}</td>
+                    <td>{{$p->name}}</td>
+                    <td>{{$p->description}}</td>
+                    <td>{{$p->price}}</td>
+                    <td>{{$p->created_at}}</td>
+                    <td>
+                        <a href="{{ route('products.show', ['product' => $p->id]) }}" class="btn btn-primary btn-sm">
+                            <i class="fa fa-eye"></i>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="{{ route('products.edit', ['product' => $p->id]) }}" class="btn btn-warning btn-sm">
+                            <i class="fa fa-edit"></i>
+                        </a>
+                    </td>
+                    <td>
+                        <form action="{{ route('products.destroy', ['product' => $p->id]) }}" method='POST'>
+                            @method('DELETE')
+                            @csrf
+                            <button class="btn btn-danger btn-sm">
+                                <i class="fa fa-remove"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+
 </html>
 
 @endsection
-
