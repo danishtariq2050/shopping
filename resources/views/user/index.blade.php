5e99f22d32fe942edb02d83b499d42d8e244933b
Muhammad Danish <muhammadd@alec.ae>
2022-05-14 01:37:16 +0500

    show image on index page of user panel

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -42,7 +42,7 @@
                 @foreach ($products as $p)
                     <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
                         <div class="featured__item">
-                            <div class="featured__item__pic set-bg" data-setbg="img/featured/feature-1.jpg">
+                            <div class="featured__item__pic set-bg" data-setbg="{{ $p->image ? asset('storage/'.$p->image) : asset('img/na.jpg')}}">
                                 <ul class="featured__item__pic__hover">
                                     <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                     <li><a href="#"><i class="fa fa-retweet"></i></a></li>
